ee344b399989fcd565be600a58c56539a69765ed
Mirakhmad Mirzajonov <m.mirakhmad@gmail.com>
2022-08-20 11:14:24 +0500

    minor fix

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -1,6 +1,6 @@
 <template> 
      <div class="mt-12 relative max-w-4xl mx-auto">
-      <div class="flex justify-end ">
+      <div class="flex justify-end mb-12">
         <button v-if="contacts.length" @click="displayModal(true);updateActionName('Добавить')" type="button" class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 <svg class="-ml-1 mr-2 h-5 w-5" x-description="Heroicon name: solid/plus" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                   <path fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd"></path>
